4d46feef99273dfc0705494e63d9d9c9f006d867
Darwin Palonpon <darwinlorenzopalonpon@gmail.com>
2014-05-19 13:04:57 +0800

    cleaned up unused code

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,8 @@
 class Task < ActiveRecord::Base
-  default_scope { order('due_date ASC') }
+  default_scope { order('created_at ASC') }
 
   belongs_to :priority
-  
+
   validates_presence_of :todo
   accepts_nested_attributes_for :priority
 end
